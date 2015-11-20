e90353947ffe8ef81308c4042eeea6e347fb93cc
bresheske <bresheske@gmail.com>
2015-11-19 19:27:31 -0600

    meh

---
+++ src/PiNet.Gpio/PiNet.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using PiNet.Gpio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PiNet.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            using (var manager = new PinManager())
+            {
+                manager.Export(Pin.PinType.GPIO18);
+                manager.Write(Pin.PinType.GPIO18, true);
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+            }
+
+        }
+    }
+}
---
+++ src/PiNet.Gpio/PiNet.Gpio/PinManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiNet.Gpio
+{
+    public class PinManager : IDisposable
+    {
+        private string GPIO_FOLDER = "/sys/class/gpio/";
+
+        private readonly IList<Pin.PinType> _activepins;
+
+        public PinManager()
+        {
+            _activepins = new List<Pin.PinType>();
+        }
+
+        public void Export(Pin.PinType pin)
+        {
+            if (_activepins.Contains(pin))
+                return;
+            var command = string.Format("echo {0} > {1}export", pin.ToString().Substring(4), GPIO_FOLDER);
+            Execute(command);
+            _activepins.Add(pin);
+        }
+
+        public void UnExport(Pin.PinType pin)
+        {
+            if (!_activepins.Contains(pin))
+                return;
+            var command = string.Format("echo {0} > {1}unexport", pin.ToString().Substring(4), GPIO_FOLDER);
+            Execute(command);
+            _activepins.Remove(pin);
+        }
+
+        public void Write(Pin.PinType pin, bool on)
+        {
+            if (!_activepins.Contains(pin))
+                return;
+            var val = on ? 1 : 0;
+            var command = string.Format("echo \"out\" > {0}gpio{1}/direction", GPIO_FOLDER, pin.ToString().Substring(4));
+            Execute(command);
+            command = string.Format("echo {0} > {1}gpio{2}/value", val, GPIO_FOLDER, pin.ToString().Substring(4));
+            Execute(command);
+        }
+
+        public bool Read(Pin.PinType pin)
+        {
+            return false;
+        }
+
+        private void Execute(string command)
+        {
+            var proc = new System.Diagnostics.Process();
+            proc.EnableRaisingEvents = false;
+            proc.StartInfo.FileName = command;
+            proc.Start();
+            proc.WaitForExit();
+        }
+
+
+
+
+
+        public void Dispose()
+        {
+            foreach(var pin in _activepins)
+            {
+                UnExport(pin);
+            }
+        }
+    }
+}
