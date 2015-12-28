f50510cf1e67d037b18416eb3b026f3e746567fd
bresheske <bresheske@gmail.com>
2015-12-28 16:26:29 -0600

    added web project

---
+++ src/PiNet.Gpio/PiNet.Gpio.Web/Controllers/GpioController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace PiNet.Gpio.Web.Controllers
+{
+    public class GpioController : ApiController
+    {
+
+        public IHttpActionResult Get()
+        {
+            return Ok(new { something = "foreveryone" });
+        }
+    }
+}
